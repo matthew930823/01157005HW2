2d06559822dc832ad716b84a69b1758b15392790
Chien Yu <leoyu93.01.04@gmail.com>
2024-10-09 10:49:13 +0800

    Initial Commit

---
+++ 01157005111/_1157005111App.swift
@@ -0,0 +1,17 @@
+//
+//  _1157005111App.swift
+//  01157005111
+//
+//  Created by user11 on 2024/10/9.
+//
+
+import SwiftUI
+
+@main
+struct _1157005111App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
